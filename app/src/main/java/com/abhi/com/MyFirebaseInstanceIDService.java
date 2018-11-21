b324e2858301f4313973fd288999785924a7f282
ain066 <pabhishekogs@gmail.com>
2018-11-21 13:25:03 +0530

    First Commit
    Second Version

--- app/src/main/java/com/abhi/com/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/abhi/com/MyFirebaseInstanceIDService.java
@@ -18,9 +18,7 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
         Log.d("Token2", "Refreshed token: " + refreshedToken);
 
         // If you want to send messages to this application instance or
-
         // manage this apps subscriptions on the server side, send the
-
         // Instance ID token to your app server.
 
     }
